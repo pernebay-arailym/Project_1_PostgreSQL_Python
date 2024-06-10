6702c1d29a252bb5ff5823212d66f8ac5afa95f2
pernebay-arailym <pernebayarailym@gmail.com>
2024-06-10 13:19:13 +0200

    insert multiple tuples

--- python_to_postgres.py
+++ python_to_postgres.py
@@ -17,6 +17,8 @@ try:
 
     cursor = connectionToSql.cursor()
 
+    cursor.execute('DROP TABLE IF EXISTS employee')
+
     create_script = ''' CREATE TABLE IF NOT EXISTS employee (
                             id      int PRIMARY KEY,
                             name    varchar(40) NOT NULL,
@@ -26,8 +28,9 @@ try:
     cursor.execute(create_script)
 
     insert_script = 'INSERT INTO employee (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
-    insert_value = (1, 'Arailym', 30000, 'D1')
-    cursor.execute(insert_script, insert_value)
+    insert_values = [(1, 'Arailym', 30000, 'D1'), (2, 'Dana', 33000, 'D1'), (3, 'Ais', 35000, 'D2')]
+    for record in insert_values:   
+        cursor.execute(insert_script, record)
 
     connectionToSql.commit()
 except Exception as error:
